615b5170c56db25ac8998b88e93e0cf2e670cd99
liuchf <642390923@qq.com>
2022-09-30 10:28:17 +0800

    feat:同步jiawawiki项目

--- test0805/src/main/java/com/test0805/demo/controller/TestController.java
+++ test0805/src/main/java/com/test0805/demo/controller/TestController.java
@@ -2,8 +2,6 @@ package com.test0805.demo.controller;
 
 import com.test0805.demo.domain.Test;
 import com.test0805.demo.service.TestService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
